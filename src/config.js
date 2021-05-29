4fd275175efd314e3cf497c136b54ef2e189e511
UploadConfigLambda <Compx341A4-21A-SeederRole-PJBO7CYOW3MA/Compx341A4-21A-UpdateConfigFunction-l9PES6o8tZ9k>
2021-05-29 17:25:09 +0000

    Updating config.js with backend variables

---
+++ src/config.js
@@ -0,0 +1,13 @@
+export default {
+  MAX_ATTACHMENT_SIZE: 5000000,
+  apiGateway: {
+    REGION: "us-east-1",
+    API_URL: "https://37erka2joi.execute-api.us-east-1.amazonaws.com/prod",
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_ImatGmU0w",
+    APP_CLIENT_ID: "1nd4d6ce8ceitohesebo2r5ded",
+    IDENTITY_POOL_ID: "us-east-1:e4079d9d-aa00-49d4-b93b-8d0dd24d7f2f"
+  }
+};
